b610c53d4c768523b67ca91924f272a3243dc659
Pablo Duchovny <pablo.duchovny@gmail.com>
2020-05-11 14:25:31 -0300

    empiezo con firebase

--- connect.js
+++ connect.js
@@ -1,57 +1,142 @@
 document.addEventListener("DOMContentLoaded", () => {
   const game = document.getElementById("game");
-  var tablero = document.createElement("div");
   var libres = [5, 5, 5, 5, 5, 5, 5];
   var jugador = 1;
   var tableroArray = [];
+  var turnoDe = document.getElementById("turnoDe");
+  var quien = "j1";
 
   function jugar(e) {
     var posicionCol = Math.floor(e.target.dataset.cf / 10);
     var posicionFila = 6 - libres[posicionCol - 1];
     var posicion = posicionCol * 10 + posicionFila;
-    console.log(posicion);
-    var quien = jugador > 0 ? "j1" : "j2";
+    //console.log(posicion);
+    quien = jugador > 0 ? "j1" : "j2";
     document.querySelector(`div[data-cf='${posicion}']`).classList.add(quien);
     libres[posicionCol - 1]--;
     jugador = jugador * -1;
+    checkWinner(quien);
+    var proximo = quien != "j1" ? "j1" : "j2";
+    turnoDe.innerHTML = proximo;
+    console.log(proximo);
+    turnoDe.classList.remove(quien);
+    turnoDe.classList.add(proximo);
+    console.log(turnoDe);
+  }
+  function ganador() {
+    console.log("capo!");
+  }
+  function checkWinner(quien) {
+    var check = quien + quien + quien + quien;
+
+    //horizontales
+    for (var f = 1; f < 6; f++) {
+      var contador = "";
+      for (var c = 1; c < 8; c++) {
+        posicion = document.querySelector(`div[data-cf='${c * 10 + f}']`);
+        if (posicion.classList.contains(quien)) {
+          contador += quien;
+        } else {
+          contador = "";
+        }
+        if (contador == check) {
+          ganador(quien);
+        }
+      }
+    }
+    //verticales
+    for (var c = 1; c < 8; c++) {
+      var contador = "";
+      for (var f = 1; f < 6; f++) {
+        posicion = document.querySelector(`div[data-cf='${c * 10 + f}']`);
+        if (posicion.classList.contains(quien)) {
+          contador += quien;
+        } else {
+          contador = "";
+        }
+        if (contador == check) {
+          ganador(quien);
+        }
+      }
+    }
+    //diagonales x=y
+    var inicios = [
+      { c: 1, f: 2 },
+      { c: 1, f: 1 },
+      { c: 2, f: 1 },
+      { c: 3, f: 1 },
+      { c: 4, f: 1 },
+    ];
+    inicios.forEach((inicio) => {
+      c = inicio.c;
+      f = inicio.f;
+      contador = "";
+      for (i = 0; i < 4; i++) {
+        posicion = document.querySelector(
+          `div[data-cf='${(c + i) * 10 + f + i}']`
+        );
+        if (posicion.classList.contains(quien)) {
+          contador += quien;
+        } else {
+          contador = "";
+        }
+        if (contador == check) {
+          ganador(quien);
+        }
+      }
+    });
+
+    //diagonales patras
+    var inicios = [
+      { c: 7, f: 2 },
+      { c: 7, f: 1 },
+      { c: 6, f: 1 },
+      { c: 5, f: 1 },
+      { c: 4, f: 1 },
+    ];
+    inicios.forEach((inicio) => {
+      c = inicio.c;
+      f = inicio.f;
+      contador = "";
+      for (i = 0; i < 4; i++) {
+        posicion = document.querySelector(
+          `div[data-cf='${(c - i) * 10 + f + i}']`
+        );
+        if (posicion.classList.contains(quien)) {
+          contador += quien;
+        } else {
+          contador = "";
+        }
+        if (contador == check) {
+          ganador(quien);
+        }
+      }
+    });
   }
 
   function armarTablero() {
     tableroArray = [];
     for (f = 5; f > 0; f--) {
       for (c = 1; c <= 7; c++) {
-        ////////////////////////////
-
-        /* function createChild() {
-    let myEle = document.createElement("span");
-    myEle.innerHTML = "hello world";
-    myEle.style.color = "blue";
-
-    let wrapper = document.getElementById("wrapper");
-    wrapper.appendChild(myEle);
- } */
-
-        /////////////////////////
-
         var casilla = document.createElement("div");
         casilla.classList.add("casilla");
         casilla.addEventListener("click", function (event) {
           jugar(event);
         });
         casilla.setAttribute("data-cf", `${c}${f}`);
-
-        casilla.innerHTML = `cf=${c}${f}`;
-
+        //casilla.innerHTML = `cf=${c}${f}`;
         game.appendChild(casilla);
         tableroArray.push(c * 10 + f);
-
-        /*  tablero += `<div class="casilla" onclick="jugar()" data-c=${c} data-c=${c} >c${c} f${f}</div>`;
-        tableroArray.push(c * 10 + f); */
       }
-      //game.innerHTML = tablero;
     }
-    console.log(tableroArray);
   }
+
+  function reset() {
+    libres = [5, 5, 5, 5, 5, 5, 5];
+    jugador = 1;
+    tableroArray = [];
+    quien = "j1";
+  }
+
   armarTablero();
-  //game.innerHTML = `<div class="casilla">1</div>`;
 });
